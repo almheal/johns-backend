6cfd57500291f04a3955fbce5dcb21af8e30e078
Dmitry <yellowblackyellow123@gmail.com>
2021-10-24 18:54:05 +0300

    add compression

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 import { swaggerInit } from './swagger/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   app.enableCors({
     origin: '*',
   });
